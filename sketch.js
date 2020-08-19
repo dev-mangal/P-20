88c20fb12dbc6de650c5275d3c56818583fb978d
dev-mangal <69078514+dev-mangal@users.noreply.github.com>
2020-08-19 14:50:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var car,wall;
+var speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(55,90);
+  weight=random(400,1500);
+  car=createSprite(50,200,50,50);
+  car.velocityX=speed; 
+  wall=createSprite(1500,200,60,height/2);
+  wall.shapeColor=color(80,80,80);
+
+}
+
+function draw() {
+  background(255);
+  car.collide(wall);
+  deformation=(0.5*weight*speed*speed)/22500;
+  if(deformation<100&&car.isTouching(wall)){
+    car.shapeColor="green";
+    car.velocityX=0; 
+  }
+  else if(deformation>100&&deformation<180&&car.isTouching(wall)){
+    car.shapeColor="yellow";
+    car.velocityX=0; 
+  }
+  else if(deformation>180&&car.isTouching(wall)){
+    car.shapeColor="red";
+    car.velocityX=0; 
+  }
+  drawSprites();
+}
